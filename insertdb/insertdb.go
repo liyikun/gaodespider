e1daa4638fff129e335df923caa27dcb9aefb535
lyk李逸琨 <liyk@ctrip.com>
2018-12-31 01:32:01 +0800

    数据库写入方法重写

--- insertdb/insertdb.go
+++ insertdb/insertdb.go
@@ -5,16 +5,23 @@ import (
 	"fmt"
 )
 
-func ConnectDb() (*sql.DB, error) {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+type ConnParam struct {
+	host     string
+	post     int
+	user     string
+	password string
+	dbname   string
+}
+
+func ConnectDb(c ConnParam) (*sql.DB, error) {
+	connStr := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+		c.host, c.post, c.user, c.password, c.dbname)
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, err
 	}
-	err = db.Ping()
-	if err != nil {
+	if err = db.Ping(); err != nil {
 		return nil, err
 	}
 	return db, nil
